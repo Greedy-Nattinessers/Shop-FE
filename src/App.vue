6659c430600891098afa4772cbc3ccfdd17748c9
theAave <115671803+theAave@users.noreply.github.com>
2024-11-21 14:32:09 +0800

    feat:添加了Element-Plus组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import {ElCarouselItem, ElCarousel} from 'element-plus'
+import 'element-plus/dist/index.css'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
+      
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
-
-  <RouterView />
+  </header> -->
+  <el-carousel :interval="4000" type="card" height="200px">
+    <el-carousel-item v-for="item in 6" :key="item">
+      <h3 text="2xl" justify="center">{{ item + 1 }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+  <!-- <RouterView /> -->
+  
 </template>
 
 <style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
